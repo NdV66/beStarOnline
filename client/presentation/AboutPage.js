aed982ed74e57c2918d2cd28ed4844f4edd23bc7
Marta Zażlak <marta.zazlak@hotmail.com>
2018-01-20 20:15:24 +0100

    improve scss

--- client/presentation/AboutPage.js
+++ client/presentation/AboutPage.js
@@ -18,45 +18,43 @@ const DescriptionAboutPage = (props) => {
 
 const ModuleAbout = (props) => {
     return(
-        <div>
+        <Row>
             <HeaderAboutPage text={props.header} />
             <DescriptionAboutPage
                 linkText={props.linkText}
                 href={props.href}
                 text={props.text} />
-        </div>
+        </Row>
         );
 };
 
 const AboutPage = () => {
     return (
             <Container>
-                <Row>
-                    <ModuleAbout
-                        header="What is beStar?"
-                        linkText="Clik here for full beStar sources."
-                        href="#"
-                        text={
-                        < span > It is a simple tool to create a text using BBcode.
-                        You can see your changes and finished work.<br />
-                        beStar is a module implemented using JavaScript.<br /> < /span>} />
+                <ModuleAbout
+                    header="What is beStar?"
+                    linkText="Clik here for full beStar sources."
+                    href="#"
+                    text={
+                    < span > It is a simple tool to create a text using BBcode.
+                    You can see your changes and finished work.<br />
+                    beStar is a module implemented using JavaScript.<br /> < /span>} />
 
-                    <ModuleAbout
-                        header="Ok, so what is BBcode?"
-                        linkText="Clik here for more information about BBcode."
-                        href="https://en.wikipedia.org/wiki/BBCode"
-                        text={
-                        < span >It's a very useful markup language.
-                        It can be using for creating post or messages. <br />
-                        The BBcode was created in 1998 and implemented in Perl. < /span>} />
+                <ModuleAbout
+                    header="Ok, so what is BBcode?"
+                    linkText="Clik here for more information about BBcode."
+                    href="https://en.wikipedia.org/wiki/BBCode"
+                    text={
+                    < span >It's a very useful markup language.
+                    It can be using for creating post or messages. <br />
+                    The BBcode was created in 1998 and implemented in Perl. < /span>} />
 
-                    <ModuleAbout
-                        header="What about beStar license?"
-                        linkText="Clik here for more information about MIT License."
-                        href="#"
-                        text={
-                        < span >beStar is under MIT License, so you can use it for free, even in commercial projects.< /span>} />
-                </Row>
+                <ModuleAbout
+                    header="What about beStar license?"
+                    linkText="Clik here for more information about MIT License."
+                    href="#"
+                    text={
+                    < span >beStar is under MIT License, so you can use it for free, even in commercial projects.< /span>} />
             </Container>
             );
 };
