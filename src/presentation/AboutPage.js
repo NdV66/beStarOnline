2ffaa81aa83b0a843c8fa537c5e47ab028a0933a
Marta Zażlak <marta.zazlak@hotmail.com>
2018-02-16 00:34:16 +0100

    add desc for syntax

--- src/presentation/AboutPage.js
+++ src/presentation/AboutPage.js
@@ -10,7 +10,8 @@ const HeaderAboutPage = (props) => {
 };
 
 const DescriptionAboutPage = (props) => {
-    return(<Col xs="12">{props.text}<AboutPageLink text={props.linkText} href={props.href} /></Col>);
+    const link = props.linkText? <AboutPageLink text={props.linkText} href={props.href} /> : null;
+    return(<Col xs="12">{props.text}{link}</Col>);
 };
 
 const ModuleAbout = (props) => {
@@ -25,6 +26,12 @@ const ModuleAbout = (props) => {
         );
 };
 
+const SyntaxTextHelpElement = (props) =>
+    <Row className="syntax-desc">
+        <Col xs="6"><span className={props.className}>{props.text}</span></Col>
+        <Col xs="6">{props.tagOpen} {props.text} {props.tagClose}</Col>
+    </Row>;
+
 const AboutPage = () => {
     return (
             <Container>
@@ -32,21 +39,25 @@ const AboutPage = () => {
                     header="What is beStar?"
                     linkText="Clik here for full beStar sources."
                     href="https://github.com/NdV66/beStarOnline"
-                    text={[ <span key="1">It is a simple tool to create a text using BBcode.</span>, <br key="5" />,
-                            <span key="1.1">You can see your changes and finished work using "Preview" button on the top of Home Page.</span>, <br key="2"/>,
-                            <span key="3">beStar is based on a module implemented using JavaScript.</span>, <br key="6" />,
-                            <span key="4">In the future will be an option to save you work (automatic or not) in localstorage and to choose a font size etc.</span>
+                    text={[ <span key="1">It is a simple tool to create a text using BBcode.</span>, <br key="5" />, <span key="1.1">You can see your changes and finished work using "Preview" button on the top of Home Page.</span>
                     ]} />
-
                 <ModuleAbout
                     header="Ok, so what is BBcode?"
                     linkText="Clik here for more information about BBcode."
                     href="https://en.wikipedia.org/wiki/BBCode"
-                    text={[<span key="1">It's a very useful markup language. It can be using for creating post or messages.</span>, <br key="2" />, <span key="3">The BBcode was created in 1998 and implemented in Perl.</span>]} />
-
+                    text={[<span key="1">It's a very useful markup language. It can be using for creating post or messages.</span>, <span key="3">The BBcode was created in 1998 and implemented in Perl.</span>]} />
                 <ModuleAbout
-                    header="What is the base syntax of BBcode (on this site)?"
-                    text={[<span key="1">Comming soon :)</span>]} />
+                    header="What is the syntax of BBcode (on this site)?"
+                    text={[
+                            <h5>Base syntax</h5>,
+                            <SyntaxTextHelpElement text={"Syntax for strong text"} className={"bb-strong"} tagOpen={"[b]"} tagClose={"[/b]"} />,
+                            <SyntaxTextHelpElement text={"Syntax for italic text"} className={"bb-italic"} tagOpen={"[i]"} tagClose={"[/i]"} />,
+                            <SyntaxTextHelpElement text={"Syntax for underlined text"} className={"bb-underlined"} tagOpen={"[u]"} tagClose={"[/u]"} />,
+                            <SyntaxTextHelpElement text={"Syntax for strikethrough text"} className={"bb-strikethrough"} tagOpen={"[s]"} tagClose={"[/s]"} />,
+                            <h5>Advanced syntax</h5>,
+                            <div>in progress :) </div>
+                        ]}
+                        />
 
                 <ModuleAbout
                     header="What about beStar license?"
@@ -59,3 +70,9 @@ const AboutPage = () => {
 };
 
 export default AboutPage;
+
+
+//                            <div key="5">[code] <code>some code here</code> [/code]</div>,
+//                            <div key="6">[url] <a href="https://google.com">https://google.com</a> [/url]</div>,
+//                            <div key="7">[url="https://google.com"] <a href="https://google.com">Description link</a> [/url]</div>,
+//                            <div key="7">[img]image url[/img]<img src="../images/bStar.jpeg" className="img-fluid" /></div>,
--- src/presentation/FooterElement.js
+++ src/presentation/FooterElement.js
@@ -6,9 +6,9 @@ const FooterElement = () => {
             <Container fluid={true}>
                 <Row>
                 <Col md="3" xs="6"><strong>Narin (NdV66)</strong></Col>
-                    <Col md="3" xs="6">Follow me on <a target="_blank" href="https://github.com/NdV66">Github</a> :)</Col>
-                    <Col md="3" xs="6" className="d-none d-md-block">under <a target="_blank" href="https://opensource.org/licenses/MIT">MIT License</a></Col>
-                    <Col md="3" xs="6" className="d-none d-md-block">v1.0.2</Col>
+                    <Col md="3" xs="6"><a target="_blank" href="https://github.com/NdV66">Github</a></Col>
+                    <Col md="3" xs="6" className="d-none d-md-block"><a target="_blank" href="https://opensource.org/licenses/MIT">MIT License</a></Col>
+                    <Col md="3" xs="6" className="d-none d-md-block">v1.0.4</Col>
                 </Row>
             </Container>
             );
